99e1ad7b1e314640fb135357d446d4b59249aa50
AlexandraDeGracia <alexandra.degracia1@utp.ac.pa>
2025-11-16 22:03:01 -0500

    Registro y Autenticación de Alumnos Avance

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador
+
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstAlumnos = new System.Windows.Forms.ListBox();
+            this.chkNotificaciones = new System.Windows.Forms.CheckBox();
+            this.chkTerminos = new System.Windows.Forms.CheckBox();
+            this.rbVespertina = new System.Windows.Forms.RadioButton();
+            this.rbMatutina = new System.Windows.Forms.RadioButton();
+            this.cmbSemestre = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbCarrera = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtConfirmar = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acerdaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstAlumnos);
+            this.groupBox1.Controls.Add(this.chkNotificaciones);
+            this.groupBox1.Controls.Add(this.chkTerminos);
+            this.groupBox1.Controls.Add(this.rbVespertina);
+            this.groupBox1.Controls.Add(this.rbMatutina);
+            this.groupBox1.Controls.Add(this.cmbSemestre);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbCarrera);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtCedula);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 260);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos del Alumno";
+            // 
+            // lstAlumnos
+            // 
+            this.lstAlumnos.Location = new System.Drawing.Point(210, 20);
+            this.lstAlumnos.Name = "lstAlumnos";
+            this.lstAlumnos.Size = new System.Drawing.Size(80, 147);
+            this.lstAlumnos.TabIndex = 0;
+            // 
+            // chkNotificaciones
+            // 
+            this.chkNotificaciones.Location = new System.Drawing.Point(150, 225);
+            this.chkNotificaciones.Name = "chkNotificaciones";
+            this.chkNotificaciones.Size = new System.Drawing.Size(104, 24);
+            this.chkNotificaciones.TabIndex = 1;
+            this.chkNotificaciones.Text = "Notificaciones";
+            // 
+            // chkTerminos
+            // 
+            this.chkTerminos.Location = new System.Drawing.Point(10, 225);
+            this.chkTerminos.Name = "chkTerminos";
+            this.chkTerminos.Size = new System.Drawing.Size(104, 24);
+            this.chkTerminos.TabIndex = 2;
+            this.chkTerminos.Text = "Acepto términos";
+            // 
+            // rbVespertina
+            // 
+            this.rbVespertina.Location = new System.Drawing.Point(10, 180);
+            this.rbVespertina.Name = "rbVespertina";
+            this.rbVespertina.Size = new System.Drawing.Size(104, 24);
+            this.rbVespertina.TabIndex = 3;
+            this.rbVespertina.Text = "Vespertina";
+            // 
+            // rbMatutina
+            // 
+            this.rbMatutina.Location = new System.Drawing.Point(120, 180);
+            this.rbMatutina.Name = "rbMatutina";
+            this.rbMatutina.Size = new System.Drawing.Size(104, 24);
+            this.rbMatutina.TabIndex = 4;
+            this.rbMatutina.Text = "Matutina";
+            // 
+            // cmbSemestre
+            // 
+            this.cmbSemestre.Location = new System.Drawing.Point(80, 140);
+            this.cmbSemestre.Name = "cmbSemestre";
+            this.cmbSemestre.Size = new System.Drawing.Size(121, 21);
+            this.cmbSemestre.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(10, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Semestre:";
+            // 
+            // cmbCarrera
+            // 
+            this.cmbCarrera.Location = new System.Drawing.Point(80, 100);
+            this.cmbCarrera.Name = "cmbCarrera";
+            this.cmbCarrera.Size = new System.Drawing.Size(121, 21);
+            this.cmbCarrera.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(10, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Carrera:";
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(80, 60);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(100, 20);
+            this.txtCedula.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(10, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Cédula:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(80, 20);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nombre:";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label10);
+            this.groupBox4.Controls.Add(this.label9);
+            this.groupBox4.Controls.Add(this.label8);
+            this.groupBox4.Controls.Add(this.txtConfirmar);
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.txtContraseña);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.txtUsuario);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Location = new System.Drawing.Point(350, 40);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(300, 200);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Credenciales";
+            // 
+            // txtConfirmar
+            // 
+            this.txtConfirmar.Location = new System.Drawing.Point(100, 110);
+            this.txtConfirmar.Name = "txtConfirmar";
+            this.txtConfirmar.Size = new System.Drawing.Size(100, 20);
+            this.txtConfirmar.TabIndex = 0;
+            this.txtConfirmar.TextChanged += new System.EventHandler(this.txtConfirmar_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(0, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 23);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Confirmar:";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(100, 70);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseña.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(0, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Contraseña:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(100, 30);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Usuario:";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.salirToolStripMenuItem,
+            this.acerdaDeToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.TabIndex = 2;
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // acerdaDeToolStripMenuItem
+            // 
+            this.acerdaDeToolStripMenuItem.Name = "acerdaDeToolStripMenuItem";
+            this.acerdaDeToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.acerdaDeToolStripMenuItem.Text = "Acerca de";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Usuario";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(21, 76);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Contrase";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(27, 117);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(51, 13);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Confirmar";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip2;
+            this.Name = "Form1";
+            this.Text = "Registro de Alumnos";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstAlumnos;
+        private System.Windows.Forms.CheckBox chkNotificaciones;
+        private System.Windows.Forms.CheckBox chkTerminos;
+        private System.Windows.Forms.RadioButton rbVespertina;
+        private System.Windows.Forms.RadioButton rbMatutina;
+        private System.Windows.Forms.ComboBox cmbSemestre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbCarrera;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtConfirmar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtUsuario;
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acerdaDeToolStripMenuItem;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+    }
+}
+
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Atajos
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+
+            // Eventos
+            txtNombre.TextChanged += txtNombre_TextChanged;
+            txtCedula.KeyPress += txtCedula_KeyPress;
+            txtNombre.Validating += txtNombre_Validating;
+            txtCedula.Validating += txtCedula_Validating;
+
+            // Menú
+            nuevoToolStripMenuItem.Click += nuevoToolStripMenuItem_Click;
+            guardarToolStripMenuItem.Click += guardarToolStripMenuItem_Click;
+            salirToolStripMenuItem.Click += salirToolStripMenuItem_Click;
+            acerdaDeToolStripMenuItem.Click += acercaDeToolStripMenuItem_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Inicio de sesión
+            string codigo = Interaction.InputBox("Ingrese el código del administrador/profesor:", "Inicio de sesión", "");
+            if (codigo != "1234")
+            {
+                MessageBox.Show("Código incorrecto. El programa se cerrará.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+                return;
+            }
+
+            // Llenar combos
+            cmbCarrera.Items.AddRange(new string[] { "Ingeniería", "Medicina", "Derecho" });
+            cmbSemestre.Items.AddRange(new string[] { "1er Semestre", "2do Semestre", "3er Semestre" });
+
+            txtContraseña.PasswordChar = '*';
+            txtContraseña.MaxLength = 12;
+            txtUsuario.ReadOnly = true;
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            GenerarUsuario();
+        }
+
+        private void txtCedula_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void txtCedula_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCedula.Text))
+            {
+                MessageBox.Show("La cédula es obligatoria.", "Advertencia");
+                e.Cancel = true;
+            }
+            else
+                GenerarUsuario();
+        }
+
+        private void txtNombre_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                MessageBox.Show("El nombre es obligatorio.", "Advertencia");
+                e.Cancel = true;
+            }
+        }
+
+        private void GenerarUsuario()
+        {
+            string nombre = txtNombre.Text.Trim();
+            string cedula = txtCedula.Text.Trim();
+
+            if (nombre.Length > 0 && cedula.Length > 0)
+            {
+                txtUsuario.Text = nombre.Substring(0, 1).ToLower() + cedula;
+            }
+            else
+            {
+                txtUsuario.Text = "";
+            }
+        }
+
+        private bool ValidarCampos()
+        {
+            if (string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                string.IsNullOrWhiteSpace(txtCedula.Text))
+            {
+                MessageBox.Show("Nombre y cédula son obligatorios.");
+                return false;
+            }
+
+            if (!chkTerminos.Checked)
+            {
+                MessageBox.Show("Debe aceptar los términos.");
+                return false;
+            }
+
+            if (txtContraseña.Text != txtConfirmar.Text)
+            {
+                MessageBox.Show("Las contraseñas no coinciden.");
+                return false;
+            }
+
+            if (txtContraseña.Text.Length < 4)
+            {
+                MessageBox.Show("La contraseña debe tener al menos 4 caracteres.");
+                return false;
+            }
+
+            if (txtUsuario.Text.Length == 0)
+            {
+                MessageBox.Show("No se pudo generar el usuario.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void GuardarAlumno()
+        {
+            if (!ValidarCampos()) return;
+
+            string jornada = rbMatutina.Checked ? "Matutina" :
+                             rbVespertina.Checked ? "Vespertina" : "No seleccionada";
+
+            string alumno = $"Nombre: {txtNombre.Text}, Cédula: {txtCedula.Text}, Usuario: {txtUsuario.Text}, " +
+                            $"Carrera: {cmbCarrera.Text}, Semestre: {cmbSemestre.Text}, Jornada: {jornada}, " +
+                            $"Notificaciones: {(chkNotificaciones.Checked ? "Sí" : "No")}";
+
+            lstAlumnos.Items.Add(alumno);
+            MessageBox.Show("Alumno registrado con éxito.", "Éxito");
+
+            LimpiarCampos();
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombre.Clear();
+            txtCedula.Clear();
+            txtUsuario.Clear();
+            txtContraseña.Clear();
+            txtConfirmar.Clear();
+            cmbCarrera.SelectedIndex = -1;
+            cmbSemestre.SelectedIndex = -1;
+            rbMatutina.Checked = false;
+            rbVespertina.Checked = false;
+            chkTerminos.Checked = false;
+            chkNotificaciones.Checked = false;
+            txtNombre.Focus();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                GuardarAlumno();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                LimpiarCampos();
+            }
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GuardarAlumno();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Desea salir?", "Salir", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Autor: Alexandra De Gracia\nVersión: 1.0", "Acerca de");
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtConfirmar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
